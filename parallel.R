37e3d5510bf177f10ebc719dda70d3fc2db418f4
Michael Chase <themikechase@gmail.com>
2024-06-28 09:46:00 -0700

    adding indent

--- parallel.R
+++ parallel.R
@@ -5,7 +5,7 @@
   library(rstanarm)
   library(dplyr)
     
-set.seed(1984)
+set.seed(1999)
 
 tic()
 
@@ -18,12 +18,12 @@ for (i in 1:3) {
   # parallel loop to fit stan_glm models
   bayes_reg <- foreach(i = 1:1, .combine = "c") %dopar% {
 
-  stan_glm(as.formula(paste("employment", "~anio")),
-           data = tasas %>% filter(gedad == unique(tasas$gedad)[1]),
-           chain = 50,
-           # prior = normal(normal_reg$coefficients[2], summary(normal_reg)$coefficients[4]),
-           refresh = 0
-  }
+    stan_glm(as.formula(paste("employment", "~anio")),
+             data = tasas %>% filter(gedad == unique(tasas$gedad)[1]),
+             chain = 50,
+             # prior = normal(normal_reg$coefficients[2], summary(normal_reg)$coefficients[4]),
+             refresh = 0
+    }
 }
 
   # stop bayes_reg function looping
